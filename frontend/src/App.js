6659c677b6a442b468f365eb4d7fd15b4555dbcc
m-triple-m <triplem656@gmail.com>
2022-07-27 19:24:47 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Link, Navigate, Route, Routes} from 'react-router-dom';
+import logo from "./logo.svg"
+import "./App.css"
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom"
 
-import Signup from './components/Signup';
-import { createTheme, ThemeProvider} from '@mui/material';
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import UploadFile from './components/Uploadfile';
-
+import Signup from "./components/Signup"
+import { createTheme, ThemeProvider } from "@mui/material"
+import { useState } from "react"
+import Navbar from "./components/Navbar"
+import Login from "./components/Login"
+import UploadFile from "./components/Uploadfile"
 
 function App() {
-  const [darkTheme, setDarkTheme]=useState(false);
-  const myLightTheme= createTheme({
+  const [darkTheme, setDarkTheme] = useState(false)
+  const myLightTheme = createTheme({
     palette: {
-      primary:{
-        main: '#fd7700'
-      }
-    }
+      primary: {
+        main: "#fd7700",
+      },
+    },
   })
-  const myDarkTheme= createTheme({
+  const myDarkTheme = createTheme({
     palette: {
-      mode: 'dark',
-      background:{
-        paper:'#333'
-      }
-    }
+      mode: "dark",
+      background: {
+        paper: "#333",
+      },
+    },
   })
   return (
     <ThemeProvider theme={darkTheme ? myDarkTheme : myLightTheme}>
       <BrowserRouter>
-      {/* <Link to="/home">Home Page </Link>
+        {/* <Link to="/home">Home Page </Link>
       <Link to="/login">Login Page</Link>    */}
-      <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
-<Routes>
-  <Route element={<Signup></Signup>}path="signup"/>
-  <Route element={<Login></Login>}path="login"/>
-  <Route element={<UploadFile />}path="uploadfile"/>
-  {/* <Route element={<Chatbox></Chatbox>}path="chatbox"/> */}
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Routes>
+          <Route element={<Signup></Signup>} path="signup" />
+          <Route element={<Login></Login>} path="login" />
+          <Route element={<UploadFile />} path="uploadfile" />
+          {/* <Route element={<Chatbox></Chatbox>}path="chatbox"/> */}
 
-  <Route element={<Navigate to="/home"/>}path="/"/>
-
-</Routes>
+          <Route element={<Navigate to="/home" />} path="/" />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
